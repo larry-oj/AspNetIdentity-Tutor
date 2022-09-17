2da9774b5ab22a7add1e74f6b5ce04fc1d7a2756
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-17 20:01:52 +0300

    logout

--- AspNetIdentity-Tutor/Pages/Account/Login.cshtml.cs
+++ AspNetIdentity-Tutor/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CookieOptions = AspNetIdentity_Tutor.Options.CookieOptions;
---
+++ AspNetIdentity-Tutor/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CookieOptions = AspNetIdentity_Tutor.Options.CookieOptions;
+
+namespace AspNetIdentity_Tutor.Pages.Account;
+
+public class Logout : PageModel
+{
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await HttpContext.SignOutAsync(CookieOptions.AuthCookieName);
+        return RedirectToPage("/Index");
+    }
+}
