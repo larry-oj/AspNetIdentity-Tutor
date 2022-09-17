d519e904fcee886e0ae81060a7c3987a7eb0828d
Larry <66905431+larry-oj@users.noreply.github.com>
2022-09-17 18:21:17 +0300

    login page

---
+++ AspNetIdentity-Tutor/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetIdentity_Tutor.Pages.Account;
+
+public class Login : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; }
+    
+    public void OnGet()
+    {
+    }
+    
+    public void OnPost()
+    {
+        
+    }
+}
+
+public class Credential
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public string Username { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
